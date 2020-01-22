b213a6c91253a1003b32118a353e2447bd1ba360
Yang Chen <chenyanghmilu@gmail.com>
2020-01-22 11:28:58 -0500

    spaces&tabs

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/flights', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
 // shortcut to mongoose.connection object
 var db = mongoose.connection;
 
 db.on('connected', function() {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
--- controllers/tickets.js
+++ controllers/tickets.js
@@ -3,36 +3,36 @@ var Ticket = require('../models/ticket');
 
 
 const newTicket = (req, res) => {
-  Flight.findById(req.params.id, function(err, flight) {
-    res.render('tickets/new', {
-      title: 'Add Ticket',
-      flight
-    });
-  })
+    Flight.findById(req.params.id, function(err, flight) {
+        res.render('tickets/new', {
+            title: 'Add Ticket',
+            flight
+        });
+    })
 };
 
 const createTicket = (req, res) => {
-	Flight.findById(req.params.id, function(err, flight) {
-    if (err || !flight) {
-      return res.redirect('/flights')
-    }
-    Ticket.create(req.body, function(err, ticket) {
-      ticket.flight = flight;
-      ticket.save(function(err) {
-        res.redirect(`/flights/${flight._id}/details`);
-      })
+    Flight.findById(req.params.id, function(err, flight) {
+        if (err || !flight) {
+            return res.redirect('/flights')
+        }
+        Ticket.create(req.body, function(err, ticket) {
+            ticket.flight = flight;
+            ticket.save(function(err) {
+                res.redirect(`/flights/${flight._id}/details`);
+            })
+        });
     });
-	});
 };
 
 const deleteTicket = (req, res) => {
-  Ticket.findByIdAndDelete(req.params.id, function(err, ticket) {
-    res.redirect(`/flights/${ticket.flight}/details`);
-  });
+    Ticket.findByIdAndDelete(req.params.id, function(err, ticket) {
+        res.redirect(`/flights/${ticket.flight}/details`);
+    });
 };
 
 module.exports = {
-  create: createTicket,
-  new: newTicket,
-  delete: deleteTicket
+    create: createTicket,
+    new: newTicket,
+    delete: deleteTicket
 };
